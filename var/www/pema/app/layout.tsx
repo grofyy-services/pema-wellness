d6a1887c722d9b2f0577184af5506d00f7f2f21e
grofyy services <grofyyservices@gmail.com>
2026-02-10 11:16:14 +0530

    Update layout.tsx

--- var/www/pema/app/layout.tsx
+++ var/www/pema/app/layout.tsx
@@ -76,6 +76,14 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
+        {/* Google Tag Manager - as high in head as possible */}
+        <Script id='gtm-head' strategy='beforeInteractive'>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-ND2SJLJD');`}
+        </Script>
         <Script id='passive-wheel-listeners' strategy='beforeInteractive'>
           {`
             // Fix for non-passive wheel event listeners
@@ -469,6 +477,16 @@ s.parentNode.insertBefore(b, s);})(window.lintrk);`}
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${crimson.variable} ${ivyOra.variable} bg-white font-crimson antialiased scroll-smooth`}
       >
+        {/* Google Tag Manager (noscript) - immediately after opening body */}
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-ND2SJLJD'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+            title='Google Tag Manager'
+          />
+        </noscript>
         <Provider>
           <SnackbarProvider maxSnack={3} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
             {!isAssessmentPage && <InfoHeader />}
